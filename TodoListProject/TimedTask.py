b533fae4fa01b028cab222b4d2389853399fb34c
gsnield <83919533+gsnield@users.noreply.github.com>
2021-09-15 21:43:18 -0400

    Finalized TimedTask. Need to test.
    Implemented averaqe_time, pause_task,
    and total_elapsed_time.  modified duration in end_task to account for days. Added boolean variable for if previously paused because head no work and this was the only thing I could think through.

--- TodoListProject/TimedTask.py
+++ TodoListProject/TimedTask.py
@@ -10,15 +10,18 @@ class TimedTask(TrackedTask):
     previous_time = datetime.time()
     time_elapsed = datetime.time()
     time_remaining = datetime.time()
+    total_time_elapsed = datetime.time()
 
     def __init__(self):
         TrackedTask.__init__(self)
         self.fastest_time = datetime.time(24, 60, 60)
-        # self.average_time = datetime.time()  # Might have to use timedelta to do this
+        self.average_time = datetime.time()  # Might have to use timedelta to do this
         self.previous_time = datetime.time()
         self.time_elapsed = datetime.time()
         self.time_remaining = datetime.time()
         self.start_time = datetime.datetime.min
+        self.total_time_elapsed = datetime.time()
+        self.previously_paused = False
 
     def get_fastest_time(self):
         return self.fastest_time
@@ -29,14 +32,28 @@ class TimedTask(TrackedTask):
     def get_average_time(self):
         return self.average_time
 
-    # def set_average_time(self, time_object):
-        # Come Back to this. Don't know if datetime allows you to simply divide a .time object
+    def set_average_time(self, time_object):
+        completions = self.get_number_of_completions()
+        if completions == 0:
+            self.average_time = time_object
+        else:
+            self.average_time = (self.average_time * completions + time_object) / (completions + 1)
 
     def get_previous_time(self):
         return self.previous_time
 
     def set_previous_time(self, time_object):
         self.previous_time = time_object
+
+    def get_total_time_elapsed(self):
+        return self.total_time_elapsed
+
+    def set_total_time_elapsed(self, time_object):
+        if self.previously_paused:
+            self.total_time_elapsed += time_object
+        else:
+            self.total_time_elapsed = time_object
+            self.previously_paused = True
 
     def start_task(self):
         # self.start_time = datetime.datetime.now().time()
@@ -54,8 +71,18 @@ class TimedTask(TrackedTask):
     def end_task(self):
         end_time = datetime.datetime.now()
         duration = end_time - self.start_time
-        if duration.time() < self.fastest_time:
-            self.set_fastest_time(duration.time())
-        self.set_previous_time(duration.time())
-        # self.set_average_time(duration.time())
+        if self.previously_paused:
+            duration += self.total_time_elapsed
+        if duration < self.fastest_time:  # Shortened to duration instead of duration.time() in order to account for day
+            self.set_fastest_time(duration)
+        self.set_previous_time(duration)
+        self.set_average_time(duration)
+        self.complete_task()
+
+    def pause_task(self):
+        self.status = self.status_tuple[2]
+        self.calculate_time_elapsed()
+        self.set_total_time_elapsed(self.time_elapsed)
+
+
 
